20e5868ee3511fd113b0f9d182b32962b2d2b8fe
StevenTheBearded <>
2021-07-24 17:09:18 -0400

    Add comments to server

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.set('port', process.env.PORT || 3001);
+const { Client } = require('pg');
+const pool = require("./db")
+
+var connectionString = "postgres://postgres:postgres@localhost:5432/database";
+const client = new Client({
+    connectionString: connectionString
+});
+
+//ROUTES
+
+app.post('/twocentadvice', async(req,res) =>{
+  try {
+    console.log(req.body)
+    
+  } catch (err) {
+    console.lerror(err.message)
+  }
+})
 
 
 app.locals.title = 'Turing Two Cents API';
@@ -15,12 +33,21 @@ app.locals.advice = [
   {id: 3, title: 'Instructor Hacks', description: 'Give them apples', upvotes: 5, date: '2020/12/01', mod: 3}
 ];
 
-app.get('/', (req, res) => {
-  const advice = app.locals.advice
+// app.get('/', (req, res) => {
+//   const advice = app.locals.advice
 
-  res.status(200).send({ advice })
-})
+//   res.status(200).send({ advice })
+// })
 
+app.get('/', function (req, res, next) {
+  client.query('SELECT * FROM twocentadvice', function (err, result) {
+      if (err) {
+          console.log(err);
+          res.status(400).send(err);
+      }
+      res.status(200).send(result.rows);
+  });
+});
 
 
 app.listen(app.get('port'), () => {console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`)});
