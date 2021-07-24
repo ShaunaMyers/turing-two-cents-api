a247bcfc59d38d87b5c2cea350a75d2abefb80f8
StevenTheBearded <>
2021-07-24 15:11:06 -0400

    Add functionality to api

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.set('port', process.env.PORT || 3001);
+
+
 app.locals.title = 'Turing Two Cents API';
+
+app.locals.advice = [
+  {id: 1, title: 'Advice Box1', description: 'This is advice box1', upvotes: 13, date: '2021/07/24', mod: 1} ,
+  {id: 2, title: 'How not to fail', description: 'Study hard buster', upvotes: 7, date: '2021/05/02', mod: 2} ,
+  {id: 3, title: 'Instructor Hacks', description: 'Give them apples', upvotes: 5, date: '2020/12/01', mod: 3}
+];
+
+app.get('/', (req, res) => {
+  const advice = app.locals.advice
+
+  res.status(200).send({ advice })
+})
 
 
 
