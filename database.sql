e08d4fd0b30aaf695d3139f8e7c1058114d90e1e
Taylor Galloway <gallowaytaylor@outlook.com>
2021-07-28 09:50:42 -0600

    Change database seeding code

--- database.sql
+++ database.sql
@@ -1,19 +1,19 @@
-CREATE TABLE turingtwocents(id SERIAL PRIMARY KEY, title VARCHAR(50), description VARCHAR(500), mod INT, upvotes INT, date VARCHAR(10)); 
+CREATE TABLE turingtwocents(id SERIAL PRIMARY KEY, title VARCHAR(50), description VARCHAR(500), mod INT, rating INT, date VARCHAR(10)); 
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Pre-teach', 'Definitely pre-teach yourself. Look ahead at lessons each day and even in the weeks ahead. This will help you on your projects. You are often not taught tech that you will need for projects', 1, 12, '07/25/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Pre-teach', 'Definitely pre-teach yourself. Look ahead at lessons each day and even in the weeks ahead. This will help you on your projects. You are often not taught tech that you will need for projects', 1, 0, '07/25/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Fetch Requests', 'Spend time playing with fetch requests and getting used to Promise.all. You will need this very early on.', 2, 31, '06/27/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Fetch Requests', 'Spend time playing with fetch requests and getting used to Promise.all. You will need this very early on.', 2, 0, '06/27/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Do not get too comfortable', 'This module will slow down and you will get complacent a bit. Do not get too used to free time. Keep the momentum up!', 3, 5, '07/15/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Do not get too comfortable', 'This module will slow down and you will get complacent a bit. Do not get too used to free time. Keep the momentum up!', 3, 0, '07/15/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Ask for help!', 'Please, please, please do not be afraid to ask for help. Ask your peers, instructors, mentor, rock...anyone and everyone that is willing to help.', 1, 17, '07/22/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Ask for help!', 'Please, please, please do not be afraid to ask for help. Ask your peers, instructors, mentor, rock...anyone and everyone that is willing to help.', 1, 0, '07/22/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Believe your cohort mates have the best intentions', 'Tensions will rise. This is a stressful experience. You will get frustrated with each other. Try your hardest to remember that people are trying their best and are also stressed: they generally DO NOT mean harm.', 2, 1, '05/18/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Believe your cohort mates have the best intentions', 'Tensions will rise. This is a stressful experience. You will get frustrated with each other. Try your hardest to remember that people are trying their best and are also stressed: they generally DO NOT mean harm.', 2, 0, '05/18/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('PD every day', 'If you set aside 30-45 minutes for PD everyay in the morning or afternoon you will be totally prepared for Mod 4 and graduation', 1, 2, '04/23/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('PD every day', 'If you set aside 30-45 minutes for PD everyay in the morning or afternoon you will be totally prepared for Mod 4 and graduation', 1, 0, '04/23/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Coding cut off time', 'Set a cut off time for coding every night. You will not get anything done after 9  or 10 PM. Spend that time winding down and getting good sleep. You will code better the next day.', 1, 3, '05/22/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Coding cut off time', 'Set a cut off time for coding every night. You will not get anything done after 9  or 10 PM. Spend that time winding down and getting good sleep. You will code better the next day.', 1, 0, '05/22/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Iterator Methods', 'If you do anything over intermission between Mods 1 and 2, memorize the iterator methods reduce, map, filter, find, and forEach. Memorize their syntax exactly and be able to say out loud what each part of the syntax does. Also know exactly what each method returns', 2, 1, '06/23/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Iterator Methods', 'If you do anything over intermission between Mods 1 and 2, memorize the iterator methods reduce, map, filter, find, and forEach. Memorize their syntax exactly and be able to say out loud what each part of the syntax does. Also know exactly what each method returns', 2, 0, '06/23/2021');
 
-INSERT INTO turingtwocents(title, description, mod, upvotes, date) VALUES ('Get to know back-enders', 'If possible, get to know the people in the back end program at least a little bit before Mod 4 as you will be working with them consistently in Mod 4.', 4, 3, '07/09/2021');
+INSERT INTO turingtwocents(title, description, mod, rating, date) VALUES ('Get to know back-enders', 'If possible, get to know the people in the back end program at least a little bit before Mod 4 as you will be working with them consistently in Mod 4.', 4, 0, '07/09/2021');
